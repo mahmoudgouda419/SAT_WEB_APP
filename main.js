9173522749892dc999a96f4ef45b370bcf604702
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-07-27 12:52:46 +0300

    add api to get the  data and print in the console

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("vocabform");
 const input = document.getElementById("vocabinput");
+const list = document.getElementById("wordlist");
 
 const words = [];
 
@@ -8,7 +9,23 @@ form.addEventListener("submit", function (e) {
   const word = input.value.trim();
   if (word === "") return;
   words.push(word);
+  getDefinition(word);
+  const li = document.createElement("li");
+  li.textContent = word;
+  list.appendChild(li);
   console.log(words);
   input.value = "";
   input.focus();
 });
+
+async function getDefinition(word) {
+  try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
